bd4c9aa17fb7258a11e913bc1ac2673e8dcdfb77
seanclapp@gmail.com <seanclapp@gmail.com@3a686271-e0b6-cd0b-7efd-0b8c28bb937d>
2013-01-14 23:37:03 +0000

    jus' some mild refactoring

--- walking-tour-savannah/src/edu/armstrong/walking_tour_savannah/TitleScreenActivity.java
+++ walking-tour-savannah/src/edu/armstrong/walking_tour_savannah/TitleScreenActivity.java
@@ -22,26 +22,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class TitleScreenActivity extends Activity {
-	HistoricSiteManager hsm;
-	Button bt;
-	
+
+	// HistoricSiteManager hsm; dont need this if it's static class, we should
+	// be able to view it everywhere
+	Button btnTours;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_title_screen);
-		
-		//TODO: Create list of sites
+
+		// TODO: Create list of sites from the xml file in raw (list of all the
+		// sites for the tours)
 		populateSites();
-		
-		bt = (Button)findViewById(R.id.buttonTours);
-		bt.getBackground().setColorFilter(0xFF00FF00, PorterDuff.Mode.MULTIPLY); //change hex values to create new colors
-	
-		bt.setOnClickListener(new View.OnClickListener() {
-			
+
+		btnTours = (Button) findViewById(R.id.buttonTours);
+		btnTours.getBackground().setColorFilter(0xFF00FF00,
+				PorterDuff.Mode.MULTIPLY);
+		// On click open the HubActivity
+		btnTours.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent HubActivityIntent = new Intent(TitleScreenActivity.this, HubActivity.class);
+				Intent HubActivityIntent = new Intent(TitleScreenActivity.this,
+						HubActivity.class);
 				startActivity(HubActivityIntent);
 			}
 		});
@@ -53,30 +56,29 @@ public class TitleScreenActivity extends Activity {
 		getMenuInflater().inflate(R.menu.activity_title_screen, menu);
 		return true;
 	}
-	
-	
-	private void populateSites(){
+
+	private void populateSites() {
 		HashMap<String, HistoricSite> listOfSites = new HashMap<String, HistoricSite>();
 		XMLParser parser = new XMLParser();
-		InputStream is = this.getResources().openRawResource(R.raw.sites); //resource must be stored in raw to work
+		InputStream is = this.getResources().openRawResource(R.raw.sites);
 		String xml = parser.getXmlFromFile(is); // getting XML
 		Log.d("xml", xml);
 		Document doc = parser.getDomElement(xml); // getting DOM element
-		
+
 		NodeList nl = doc.getElementsByTagName("site");
-	    
+
 		// looping through all sites
-	    for (int i = 0; i < nl.getLength(); i++) {
-	    	Element e = (Element) nl.item(i);
-	    	
-	        String name = parser.getValue(e, "name");
-	        double lat = Double.parseDouble(parser.getValue(e, "lat"));
-	        double lon = Double.parseDouble(parser.getValue(e, "lon"));
-	            
-	        Log.d("Added site", name);
-	        listOfSites.put(name, new HistoricSite(name, new GeoPoint((int)(lat * 1E6) , (int)(lon * 1E6))));
-	    }
-		
+		for (int i = 0; i < nl.getLength(); i++) {
+			Element e = (Element) nl.item(i);
+
+			String name = parser.getValue(e, "name");
+			double lat = Double.parseDouble(parser.getValue(e, "lat"));
+			double lon = Double.parseDouble(parser.getValue(e, "lon"));
+
+			Log.d("Added site", name);
+			listOfSites.put(name, new HistoricSite(name, new GeoPoint(
+					(int) (lat * 1E6), (int) (lon * 1E6))));
+		}
 		new HistoricSiteManager(listOfSites);
 	}
 }
