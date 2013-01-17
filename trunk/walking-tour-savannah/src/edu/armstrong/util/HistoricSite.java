6aa799538399d8a82e2006ced3b250e6f3547082
Luckless.am.i@gmail.com <Luckless.am.i@gmail.com@3a686271-e0b6-cd0b-7efd-0b8c28bb937d>
2013-01-17 03:23:41 +0000

    added images for two sites in the xml. dynamically loads in the info panes =))) Try the same for text

--- trunk/walking-tour-savannah/src/edu/armstrong/util/HistoricSite.java
+++ trunk/walking-tour-savannah/src/edu/armstrong/util/HistoricSite.java
@@ -7,16 +7,18 @@ import com.google.android.gms.maps.model.LatLng;
  * @author Sean Clapp, Dakota Brown
  * @since 01/16/13
  * 
- * Holds the name and GeoPoint of a dig site
+ * Holds the name and LatLng of a dig site
  * 
  */
 public class HistoricSite {
 	private String name;
 	private LatLng ll;
+	private String img;
 
-	public HistoricSite(String name, LatLng ll){
+	public HistoricSite(String name, LatLng ll, String img){
 		this.name = name;
 		this.ll = ll;
+		this.img = img;
 	}
 
 	public String getName() {
@@ -34,4 +36,14 @@ public class HistoricSite {
 	public void setGp(LatLng ll) {
 		this.ll = ll;
 	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+	
+	
 }
--- trunk/walking-tour-savannah/src/edu/armstrong/walking_tour_savannah/MapOfHistoricPointsActivity.java
+++ trunk/walking-tour-savannah/src/edu/armstrong/walking_tour_savannah/MapOfHistoricPointsActivity.java
@@ -3,11 +3,23 @@ package edu.armstrong.walking_tour_savannah;
 import java.util.HashMap;
 
 import android.annotation.TargetApi;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.internal.r;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.maps.MapActivity;
 
@@ -31,9 +43,9 @@ public class MapOfHistoricPointsActivity extends MapActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
 
-		//Requires API 11- NEED TO DECIDE!!!
+		// Requires API 11- NEED TO DECIDE!!!
 		setUpMapIfNeeded();
-		 
+
 	}
 
 	@Override
@@ -41,27 +53,73 @@ public class MapOfHistoricPointsActivity extends MapActivity {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
-	private void setUpMapIfNeeded() {
-	    // Do a null check to confirm that we have not already instantiated the map.
-	    if (map == null) {
-	        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
-	                            .getMap();
-	        // Check if we were successful in obtaining the map.
-	        if (map != null) {
-	            // The Map is verified. It is now safe to manipulate the map.
-	        	map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-	        	
-	        	HashMap<String, HistoricSite> listOfSites = new HashMap<String, HistoricSite>();
-	    		listOfSites = HistoricSiteManager.getInstanceOf().getListOfSites();
-	    		
-	    		for(HistoricSite hs : listOfSites.values()){
-	    			map.addMarker(new MarkerOptions()
-	    	        .position(hs.getGp())
-	    	        .title(hs.getName()));
-	    		}
 
-	        }
-	    }
+	private void setUpMapIfNeeded() {
+		// Do a null check to confirm that we have not already instantiated the
+		// map.
+		if (map == null) {
+			map = ((MapFragment) getFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			// Check if we were successful in obtaining the map.
+			if (map != null) {
+				// The Map is verified. It is now safe to manipulate the map.
+				map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+				HashMap<String, HistoricSite> listOfSites = new HashMap<String, HistoricSite>();
+				listOfSites = HistoricSiteManager.getInstanceOf()
+						.getListOfSites();
+
+				for (HistoricSite hs : listOfSites.values()) {
+					map.addMarker(new MarkerOptions().position(hs.getGp())
+							.title(hs.getName()));
+				}
+
+				map.setInfoWindowAdapter(new InfoWindowAdapter() {
+
+					private final View markerInfo = getLayoutInflater()
+							.inflate(R.layout.marker_info_layout, null);
+
+					@Override
+					public View getInfoWindow(Marker marker) {
+						return null;
+					}
+
+					@Override
+					public View getInfoContents(Marker marker) {
+						String title = marker.getTitle();
+						HistoricSite hs = HistoricSiteManager.getInstanceOf()
+								.getListOfSites().get(title);
+						
+						Resources res = getResources();
+						String imgName = hs.getImg();
+						int resID = res.getIdentifier(imgName, "drawable", getPackageName());
+						Drawable drawable = res.getDrawable(resID);
+						
+						ImageView iv = ((ImageView) markerInfo.findViewById(R.id.ivInfoWindowMain));
+					    iv.setImageDrawable(drawable);
+								
+						TextView txtTitle = ((TextView) markerInfo
+								.findViewById(R.id.txtInfoWindowTitle));
+
+						if (title != null) {
+							SpannableString titleText = new SpannableString(title);
+							titleText.setSpan(
+									new ForegroundColorSpan(Color.RED), 0,
+									titleText.length(), 0);
+
+							txtTitle.setText(titleText);
+
+						} else {
+							txtTitle.setText("");
+						}
+
+						TextView txtType = ((TextView) markerInfo.findViewById(R.id.txtInfoWindowEventType));
+						txtType.setText("We'll put more details laterrr!");
+
+						return markerInfo;
+					}
+				});
+			}
+		}
 	}
 }
