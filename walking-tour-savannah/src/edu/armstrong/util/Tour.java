0972ff36f5ad6e355dcdae60c73b4d49d53bba6f
seanclapp@gmail.com <seanclapp@gmail.com@3a686271-e0b6-cd0b-7efd-0b8c28bb937d>
2013-02-06 23:40:14 +0000

    Fixed some formatting issues with the site rows. (text wasn't wrapping, etc.) Fixed tour sites to wrap entire width. Added an image to the tours, updated the populateTours() in the title activity to reflect this change.

--- walking-tour-savannah/src/edu/armstrong/util/Tour.java
+++ walking-tour-savannah/src/edu/armstrong/util/Tour.java
@@ -2,15 +2,20 @@ package edu.armstrong.util;
 
 import java.util.LinkedList;
 
+import android.graphics.drawable.Drawable;
+
 public class Tour {
 	private LinkedList<HistoricSite> tourRoute;
 	private String tourName;
 	private String tourDesc;
+	private Drawable img;
 	
-	public Tour(String tourName, String tourDesc, LinkedList<HistoricSite> tourRoute){
+
+	public Tour(String tourName, String tourDesc, LinkedList<HistoricSite> tourRoute, Drawable img){
 		this.tourName = tourName;
 		this.tourDesc = tourDesc;
 		this.tourRoute = tourRoute;
+		this.img = img;
 	}
 	
 	public LinkedList<HistoricSite> getTourRoute() {
@@ -31,4 +36,11 @@ public class Tour {
 	public void setTourDesc(String tourDesc) {
 		this.tourDesc = tourDesc;
 	}
+	public Drawable getImg() {
+		return img;
+	}
+	public void setImg(Drawable img) {
+		this.img = img;
+	}
+
 }
